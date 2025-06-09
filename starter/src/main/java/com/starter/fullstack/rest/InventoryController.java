ad3647a47dc597f75dd1b39d801e5b97c409b766
Casey Lee <caseylee@Caseys-MacBook-Pro.local>
2025-06-09 12:41:05 -0400

    Added post for the InventoryController

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -5,6 +5,7 @@ import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,11 @@ public class InventoryController {
   public InventoryController(InventoryDAO inventoryDAO) {
     Assert.notNull(inventoryDAO, "Inventory DAO must not be null.");
     this.inventoryDAO = inventoryDAO;
+  }
+
+  @PostMapping("/Create")
+  public Inventory Create(Inventory inventory) {
+    return this.inventoryDAO.create(inventory);
   }
 
   /**
