01e3c3cc3cb1c4ee7c14fea7a7be84bae251899d
Casey Lee <caseylee@Caseys-MacBook-Pro.local>
2025-06-09 12:42:00 -0400

    Added test for post in Inventory Controller

---
+++ starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
@@ -0,0 +1,56 @@
+package com.starter.fullstack.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.starter.fullstack.api.Inventory;
+import com.starter.fullstack.dao.InventoryDAO;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class InventoryControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private MongoTemplate mongoTemplate;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private InventoryDAO inventorydao;
+  private Inventory inventory;
+
+  @Before
+  public void setup() throws Throwable {
+    this.inventorydao = new InventoryDAO(this.mongoTemplate);
+
+  }
+
+  @Test
+  public void create() throws Throwable {
+    this.inventory = new Inventory();
+    this.inventory.setName("Test");
+    this.inventory.setProductType("TestProduct");
+    this.mockMvc.perform(post("/inventory/Create")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(this.objectMapper.writeValueAsString(this.inventory)))
+      .andExpect(status().isOk());
+
+    Assert.assertEquals(1, inventorydao.findAll().size());
+  }
+}
