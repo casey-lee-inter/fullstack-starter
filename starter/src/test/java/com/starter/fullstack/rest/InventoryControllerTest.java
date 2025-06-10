3ae5754db40a9a9151ec95e9081cf3ff4f8cb044
Casey Lee <caseylee@Caseys-MacBook-Pro.local>
2025-06-10 08:01:36 -0400

    Completed proper tests of the Inventory Controller

--- starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
+++ starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
@@ -3,6 +3,7 @@ package com.starter.fullstack.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.starter.fullstack.api.Inventory;
 import com.starter.fullstack.dao.InventoryDAO;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,8 +15,11 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,7 +41,11 @@ public class InventoryControllerTest {
   @Before
   public void setup() throws Throwable {
     this.inventorydao = new InventoryDAO(this.mongoTemplate);
+  }
 
+  @After
+  public void tearDown() {
+    this.mongoTemplate.dropCollection(Inventory.class);
   }
 
   @Test
@@ -45,12 +53,29 @@ public class InventoryControllerTest {
     this.inventory = new Inventory();
     this.inventory.setName("Test");
     this.inventory.setProductType("TestProduct");
-    this.mockMvc.perform(post("/inventory/Create")
+    this.mockMvc.perform(post("/inventory/create")
         .accept(MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON)
         .content(this.objectMapper.writeValueAsString(this.inventory)))
       .andExpect(status().isOk());
-
+    int i = inventorydao.findAll().size();
     Assert.assertEquals(1, inventorydao.findAll().size());
+  }
+
+  @Test
+  public void delete_test() throws Throwable {
+    this.inventory = new Inventory();
+    this.inventory.setName("Test");
+    this.inventory.setProductType("TestProduct");
+    int i = inventorydao.findAll().size();
+    String id = this.inventorydao.create(this.inventory).getId();
+    int size = inventorydao.findAll().size();
+    this.mockMvc.perform(delete("/inventory/delete")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .param("id", id))
+      .andExpect(status().isOk());
+
+    Assert.assertEquals(size - 1, inventorydao.findAll().size());
   }
 }
